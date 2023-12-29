d22d6e75533da2dd3d83ab4c16ac343e11fd54ab
Dragos Carmaciu <dragman@gmail.com>
2023-12-29 12:03:26 +0000

    Add gui Add homepage

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
 import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
 import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
 
+import { addLabelToMesh } from "./gui";
+
 // Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
@@ -18,10 +20,10 @@ const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas);
 
 // Create our first scene.
-var scene = new Scene(engine);
+const scene = new Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 
 // This targets the camera to scene origin
 camera.setTarget(Vector3.Zero());
@@ -30,25 +32,27 @@ camera.setTarget(Vector3.Zero());
 camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
 light.intensity = 0.7;
 
 // Create a grid material
-var material = new GridMaterial("grid", scene);
+const material = new GridMaterial("grid", scene);
 
 // Our built-in 'sphere' shape.
-var sphere = CreateSphere('sphere1', { segments: 16, diameter: 2 }, scene);
-
+const sphere = CreateSphere('sphere1', { segments: 16, diameter: 2 }, scene);
 // Move the sphere upward 1/2 its height
 sphere.position.y = -2;
+
 
 // Affect a material
 sphere.material = material;
 
+addLabelToMesh(sphere);
+
 // Our built-in 'ground' shape.
-var ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, scene);
+const ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, scene);
 
 // Affect a material
 ground.material = material;
