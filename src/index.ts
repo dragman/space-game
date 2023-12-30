1f514a0d281d07689ffb600d02ae7165e5d32203
Dragos Carmaciu <dragman@gmail.com>
2023-12-30 17:16:44 +0000

    Make ground pickable Add concept of pickable mesh Use prettier

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,128 @@
-import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
-import { Engine } from '@babylonjs/core/Engines/engine';
-import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
-import { Vector3 } from '@babylonjs/core/Maths/math.vector';
-import { CreateGround } from '@babylonjs/core/Meshes/Builders/groundBuilder';
-import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
-import { Scene } from '@babylonjs/core/scene';
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
+import { Scene } from "@babylonjs/core/scene";
 
-import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
+import { GridMaterial } from "@babylonjs/materials/grid/gridMaterial";
 
 // Augments the scene with the debug methods
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 
 import { addLabelToMesh } from "./gui";
+import { Behavior, Mesh, PointerEventTypes, PointerInfo, int } from "@babylonjs/core";
 
-const canvas_ = document.getElementById("renderCanvas") as HTMLCanvasElement;
-const engine_ = new Engine(canvas_);
-const scene_ = new Scene(engine_);
-
-const onResize = () => {
-    engine_.resize();
+interface IPickableMesh {
+    mesh: Mesh;
+    onPicked(pointerInfo: PointerInfo): void;
 }
 
-const onIsReady = async (scene: Scene): Promise<void> => {
-    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-    camera.setTarget(Vector3.Zero());
-    camera.attachControl(canvas_, true);
+class PickableMeshBehaviour implements Behavior<IPickableMesh> {
+    public name: string;
+    private pickable: IPickableMesh;
+    private scene: Scene;
 
-    const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-
-    const material = new GridMaterial("grid", scene);
-
-    const sphere = CreateSphere('sphere1', { segments: 16, diameter: 2 }, scene);
-    sphere.position.y = 1;
-    sphere.material = material;
-
-    addLabelToMesh(sphere);
-
-    const ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, scene);
-    ground.material = material;
-
-    if (IS_DEVELOPMENT) {
-        scene.debugLayer.show();
+    constructor(name: string, scene: Scene) {
+        this.name = name;
+        this.scene = scene;
     }
+
+    public init(): void {}
+
+    public attach(pickable: IPickableMesh): void {
+        this.pickable = pickable;
+        this.scene.onPointerObservable.add(this.onPointerPick);
+    }
+
+    public detach(): void {
+        this.scene.onPointerObservable.removeCallback(this.onPointerPick);
+    }
+
+    private onPointerPick = (pointerInfo: PointerInfo) => {
+        switch (pointerInfo.type) {
+            case PointerEventTypes.POINTERPICK:
+                if (pointerInfo.pickInfo.pickedMesh === this.pickable.mesh) {
+                    this.pickable.onPicked(pointerInfo);
+                }
+                break;
+            default:
+                break;
+        }
+    };
 }
 
-window.addEventListener("resize", onResize);
+class Grid implements IPickableMesh {
+    public gridSize: int;
+    public gridSubdivisions: int;
+    public mesh: Mesh;
 
-if (scene_.isReady()) {
-    onIsReady(scene_);
+    constructor(name: string, scene: Scene, size: int, subdivisions: int) {
+        this.gridSize = size;
+        this.gridSubdivisions = subdivisions;
+
+        const material = new GridMaterial("grid", scene);
+        material.gridRatio = 3;
+        material.gridRatio = this.gridSize / this.gridSubdivisions;
+
+        this.mesh = CreateGround("ground1", { width: size, height: size, subdivisions: subdivisions }, scene);
+
+        this.mesh.material = material;
+    }
+
+    onPicked = (pointerInfo: PointerInfo): void => {
+        alert("BAM!");
+    };
 }
 
-engine_.runRenderLoop(() => {
-    scene_.render();
-});
+class BabylonApp {
+    private engine: Engine;
+    private scene: Scene;
+    private canvas: HTMLCanvasElement;
+
+    constructor(canvasId: string) {
+        this.canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+        this.engine = new Engine(this.canvas);
+        this.scene = new Scene(this.engine);
+    }
+
+    onResize = (): void => {
+        this.engine.resize();
+    };
+
+    onIsReady = async (): Promise<void> => {
+        const camera = new ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 5, 10, Vector3.Zero(), this.scene);
+        camera.setTarget(Vector3.Zero());
+        camera.attachControl(this.canvas, true);
+
+        const light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
+        light.intensity = 0.7;
+
+        const ground = new Grid("ground1", this.scene, 12, 4);
+        addLabelToMesh(ground.mesh);
+
+        const pickableBehaviour = new PickableMeshBehaviour("pickableMeshBehaviour1", this.scene);
+        pickableBehaviour.attach(ground);
+
+        if (IS_DEVELOPMENT) {
+            this.scene.debugLayer.show();
+        }
+    };
+
+    run = (): void => {
+        window.addEventListener("resize", this.onResize);
+
+        if (this.scene.isReady()) {
+            this.onIsReady();
+        }
+
+        this.scene.onPointerObservable.add();
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+    };
+}
+
+const app = new BabylonApp("renderCanvas").run();
